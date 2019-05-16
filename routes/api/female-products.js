7bcc9375d67020ea6845327508e606ff330c3df5
Marek Bareła <marek.barela@wp.pl>
2019-05-16 20:44:36 +0200

    routes update

--- routes/api/female-products.js
+++ routes/api/female-products.js
@@ -23,6 +23,138 @@ router.get('/dress', (req, res) => {
     .then(products => res.json(products))
 });
 
+// @route GET api/female/products/tunic
+// @desc get tunic products
+// @access Public 
+
+router.get('/tunic', (req, res) => {
+  FemaleProduct.find({
+      productType: "tunic",
+      productGroup: "clothes"
+    })
+    .then(products => res.json(products))
+});
+
+// @route GET api/female/products/blouses
+// @desc get blouses products
+// @access Public 
+
+router.get('/blouses', (req, res) => {
+  FemaleProduct.find({
+      productType: "blouses",
+      productGroup: "clothes"
+    })
+    .then(products => res.json(products))
+});
+
+// @route GET api/female/products/skirt
+// @desc get skirt products
+// @access Public 
+
+router.get('/skirt', (req, res) => {
+  FemaleProduct.find({
+      productType: "skirt",
+      productGroup: "clothes"
+    })
+    .then(products => res.json(products))
+});
+
+// @route GET api/female/products/pins
+// @desc get pins products
+// @access Public 
+
+router.get('/pins', (req, res) => {
+  FemaleProduct.find({
+      productType: "pins",
+      productGroup: "boots"
+    })
+    .then(products => res.json(products))
+});
+
+// @route GET api/female/products/booties
+// @desc get booties products
+// @access Public 
+
+router.get('/booties', (req, res) => {
+  FemaleProduct.find({
+      productType: "booties",
+      productGroup: "boots"
+    })
+    .then(products => res.json(products))
+});
+
+// @route GET api/female/products/ballerina
+// @desc get ballerina products
+// @access Public 
+
+router.get('/ballerina', (req, res) => {
+  FemaleProduct.find({
+      productType: "ballerina",
+      productGroup: "boots"
+    })
+    .then(products => res.json(products))
+});
+
+// @route GET api/female/products/tracksuit
+// @desc get tracksuit products
+// @access Public 
+
+router.get('/tracksuit', (req, res) => {
+  FemaleProduct.find({
+      productType: "tracksuit",
+      productGroup: "sport"
+    })
+    .then(products => res.json(products))
+});
+
+// @route GET api/female/products/tshirt
+// @desc get tshirt products
+// @access Public 
+
+router.get('/tshirt', (req, res) => {
+  FemaleProduct.find({
+      productType: "tshirt",
+      productGroup: "sport"
+    })
+    .then(products => res.json(products))
+});
+
+// @route GET api/female/products/bags
+// @desc get bags products
+// @access Public 
+
+router.get('/bags', (req, res) => {
+  FemaleProduct.find({
+      productType: "bags",
+      productGroup: "accesories"
+    })
+    .then(products => res.json(products))
+});
+
+// @route GET api/female/products/watches
+// @desc get watches products
+// @access Public 
+
+router.get('/watches', (req, res) => {
+  FemaleProduct.find({
+      productType: "watches",
+      productGroup: "accesories"
+    })
+    .then(products => res.json(products))
+});
+
+// @route GET api/female/products/jewelry
+// @desc get jewelry products
+// @access Public 
+
+router.get('/jewelry', (req, res) => {
+  FemaleProduct.find({
+      productType: "jewelry",
+      productGroup: "accesories"
+    })
+    .then(products => res.json(products))
+});
+
 // @route GET api/female/products/:id
 // @desc get single product
 // @access Public 
